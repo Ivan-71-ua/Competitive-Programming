a49f6e392f026bf42ca4305ec871ef9c91298e23
HROZNY17 <ivansymochko2006@gmail.com>
2025-07-09 00:07:32 +0300

    LeetCode Problem 1751

---
+++ LeetCode/1751.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+
+class Solution {
+public:
+	int maxValue(std::vector<std::vector<int>>& events, int k) {
+		int n = events.size();
+		std::stable_sort(events.begin(), events.end());
+		std::vector<std::vector<int>> dp(n + 1, std::vector<int>(k + 1, 0));
+		for (int i = 1; i <= n; i++) {
+			int prev = binarySearch(events, events[i - 1][0]);
+			for (int j = 1; j <= k; j++) {
+				dp[i][j] = std::max(dp[i - 1][j], dp[prev + 1][j - 1] + events[i - 1][2]);
+			}
+		}
+		return dp[n][k];
+	}
+	int binarySearch(std::vector<std::vector<int>>& events, int start) {
+		int res = -1, l = 0, r = events.size() - 1;
+		while(l <= r) {
+			int mid = l + (r - l) / 2;
+			if(events[mid][1] < start) {
+				res = mid;
+				l = mid + 1;
+			} else {
+				r = mid - 1;
+			}
+		}
+		return res;
+	}
+};
